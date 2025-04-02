95099ab39c61a0081aaee2dc49dce4fff656ff87
Abegail-Magaling <abegail_magaling@dlsu.edu.ph>
2025-04-03 06:46:12 +0800

    LAST UPDATE

--- config/db.js
+++ config/db.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 //const connect = mongoose.connect("mongodb://localhost:27017/KarFind_DB");
 const connect = mongoose.connect(process.env.MONGO_URI);
 
+console.log(process.env.MONGO_URI);
+
 //check database connections
 connect.then(()=> {
     console.log("Database connected successfully");
